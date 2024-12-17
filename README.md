53fb2512a868cf6d63dfc085fc8bb2e1178b6698
schpet1 <schpet1@pax.priv.no>
2024-12-17 16:47:44 +0100

    initial

---
+++ README.md
@@ -0,0 +1,2 @@
+# Bug with PrimeNG virtual scroll and scrollHeight flex
+Remove line 15 and 16 in app.component.html and it will work without virtual scrolling.
---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { TableModule } from "primeng/table";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [TableModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'ng-app';
+
+  // generate 1000 random customers
+  customers: {name: string; country: string; company: string;}[] = Array.from({length: 10_000}, (_,index) => generateCustomer(index));
+
+}
+
+
+const generateCustomer = (index: number) => {
+  const countries = ['USA', 'Germany', 'Japan'];
+  const companies = ['Apple', 'BMW', 'Sony', 'Ford'];
+  return {
+    index,
+    name: Math.random().toString(36).substring(7),
+    country: countries[Math.floor(Math.random() * countries.length)],
+    company: companies[Math.floor(Math.random() * companies.length)]
+  };
+}
